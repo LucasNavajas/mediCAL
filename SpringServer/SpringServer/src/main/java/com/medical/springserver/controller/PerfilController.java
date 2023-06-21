026b21dae573de53877fab63435907b4fed655e9
MirandaGomezM <107820887+MirandaGomezM@users.noreply.github.com>
2023-06-21 17:16:12 -0300

    commit perfil y perfilpermiso
    controll, model y test de Perfil PerfilPermiso

---
+++ SpringServer/SpringServer/src/main/java/com/medical/springserver/controller/PerfilController.java
@@ -0,0 +1,28 @@
+package com.medical.springserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.medical.springserver.model.perfil.Perfil;
+import com.medical.springserver.model.perfil.PerfilDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+
+@RestController
+public class PerfilController {
+
+	@Autowired
+	PerfilDao perfilDao;
+	
+	@GetMapping("/perfil/get-all")
+	public List<Perfil> getAllPerfiles(){
+		return perfilDao.getAllPerfiles();
+	}
+	
+	@PostMapping("/perfil/save")
+	public Perfil save(@RequestBody Perfil perfil){
+		return perfilDao.save(perfil);
+	}
+}
