e0229e3abac8dd8c1a788fe100ba173e9e8d8dc7
MirandaGomezM <107820887+MirandaGomezM@users.noreply.github.com>
2023-06-21 19:18:49 -0300

    Revert "commit codigoverificacion"
    This reverts commit 82068e1b9ad9e9dd6d0d074a2bb98b5b83a84d4f.

--- SpringServer/SpringServer/src/main/java/com/medical/springserver/model/codigoverificacion/CodigoVerificacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.medical.springserver.model.codigoverificacion;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CodigoVerificacionRepository extends CrudRepository<CodigoVerificacion, Integer>{
-
-}
