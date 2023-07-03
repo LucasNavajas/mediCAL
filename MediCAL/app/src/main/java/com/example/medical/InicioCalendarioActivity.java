561b134281b3ae6cfc1173b00281e0cd44d8708e
Lucas Navajas <61892711+LucasNavajas@users.noreply.github.com>
2023-07-02 21:04:00 -0300

    Inicio calendario, tema nocturno, arreglo en agrega_correctamente para correr la app

---
+++ MediCAL/app/src/main/java/com/example/medical/InicioCalendarioActivity.java
@@ -0,0 +1,25 @@
+package com.example.medical;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+
+public class InicioCalendarioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_calendario);
+        ImageView menuButton = findViewById(R.id.menu_button);
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+}
