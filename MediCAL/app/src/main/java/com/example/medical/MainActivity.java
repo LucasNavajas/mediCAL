4b4050423f6b1b07fc752a8868e0c7c0216af83b
Lucas Navajas <61892711+LucasNavajas@users.noreply.github.com>
2023-07-07 14:21:56 -0300

    25.2, 26, 26.1, 26.2, 26.3

--- MediCAL/app/src/main/java/com/example/medical/MainActivity.java
+++ MediCAL/app/src/main/java/com/example/medical/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, InicioCalendarioActivity.class); //cambiar BienvenidoActivity por CalendarioParaquien
+                Intent intent = new Intent(MainActivity.this, BienvenidoActivity.class); //cambiar BienvenidoActivity por CalendarioParaquien
                 startActivity(intent);
                 finish();
             }
