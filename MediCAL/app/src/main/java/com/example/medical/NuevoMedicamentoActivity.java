b7890cc96031a134a1356d21cf654e56ae8c72a8
Lucas Navajas <61892711+LucasNavajas@users.noreply.github.com>
2023-08-18 15:31:02 -0300

    captura de fotos fotoMedicamento

--- MediCAL/app/src/main/java/com/example/medical/NuevoMedicamentoActivity.java
+++ MediCAL/app/src/main/java/com/example/medical/NuevoMedicamentoActivity.java
@@ -1,13 +1,17 @@
 package com.example.medical;
 
 import android.app.Activity;
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -19,6 +23,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 
@@ -30,6 +35,7 @@ public class NuevoMedicamentoActivity extends AppCompatActivity {
     private EditText nombreMedicamento;
     private EditText marcaMedicamento;
 
+    private File capturedPhotoFile;  // Declare the File object
     private ActivityResultLauncher<Intent> galleryLauncher;
     private ActivityResultLauncher<String> cameraPermissionLauncher;
 
@@ -38,6 +44,12 @@ public class NuevoMedicamentoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.n32_anadir_medicina);
         inicializarVariables();
+
+        capturedPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "captured_photo.jpg");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_IMAGE_CAPTURE);
+        }
 
         cameraPermissionLauncher = registerForActivityResult(
                 new ActivityResultContracts.RequestPermission(),
@@ -54,21 +66,20 @@ public class NuevoMedicamentoActivity extends AppCompatActivity {
                 result -> {
                     if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
                         Intent intent = result.getData();
-                        if (intent.getAction() != null && intent.getAction().equals(MediaStore.ACTION_IMAGE_CAPTURE)) {
-                            // Se tomó una foto con la cámara
-                            Bundle extras = intent.getExtras();
-                            if (extras != null) {
-                                Bitmap imgBitmap = (Bitmap) extras.get("data");
+                        if (intent.getAction() == null) {
+                            // Load the captured image from the file
+                            Bitmap imgBitmap = BitmapFactory.decodeFile(capturedPhotoFile.getAbsolutePath());
+                            if (imgBitmap != null) {
                                 fotoMedicamento.setImageBitmap(imgBitmap);
                             }
                         } else {
                             // Se seleccionó una imagen de la galería
                             Uri selectedImageUri = intent.getData();
+                            // Establecer la imagen seleccionada en el ImageView
                             fotoMedicamento.setImageURI(selectedImageUri);
                         }
                     }
                 });
-
     }
 
     private void inicializarVariables() {
@@ -85,14 +96,22 @@ public class NuevoMedicamentoActivity extends AppCompatActivity {
         });
     }
     private void abrirSelectorImagen() {
-        Intent pickImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Si ya tienes el permiso de la cámara, abre el selector de imágenes
+            Intent pickImageIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+            Uri photoUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", capturedPhotoFile);
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
 
-        Intent chooserIntent = Intent.createChooser(pickImageIntent, "Seleccionar imagen o tomar una foto");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{takePhotoIntent});
+            Intent chooserIntent = Intent.createChooser(pickImageIntent, "Seleccionar imagen o tomar una foto");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{takePhotoIntent});
 
-        galleryLauncher.launch(chooserIntent);
+            galleryLauncher.launch(chooserIntent);
+        } else {
+            // Si no tienes el permiso, solicítalo
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
     }
 
 }
