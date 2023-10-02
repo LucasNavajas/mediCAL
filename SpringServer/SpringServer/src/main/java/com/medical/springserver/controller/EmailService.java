5dec78a0978ab6eba637194923358405fc8cb1bd
Lucas Navajas <61892711+LucasNavajas@users.noreply.github.com>
2023-10-02 19:22:32 -0300

    arreglos eclipse 2.0

--- SpringServer/SpringServer/src/main/java/com/medical/springserver/controller/EmailService.java
+++ SpringServer/SpringServer/src/main/java/com/medical/springserver/controller/EmailService.java
@@ -1,8 +1,10 @@
 package com.medical.springserver.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EmailService {
